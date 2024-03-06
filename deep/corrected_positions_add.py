d91378ee825ccbeb4f78d23052e0d49ad3e814d1
Kevin Balem <kevin.balem@ifremer.fr>
2024-03-06 14:11:35 +0100

    upd

--- deep/corrected_positions_add.py
+++ deep/corrected_positions_add.py
@@ -5,9 +5,11 @@ import pandas as pd
 import gsw, os
 import copernicus_marine_client as copernicus_marine
 
-def get_coords_corrected_cli(du):    
+def get_coords_corrected_glo(du):    
     # Set parameters
-    ds_id = "cmems_mod_glo_phy_my_0.083deg-climatology_P1M-m"    
+    ds_id = "cmems_mod_glo_phy-cur_anfc_0.083deg_P1D-m"
+    
+    #print("Downloading velocities on CMEMS : ",ds_id)
     # Load xarray dataset
     vel_glo = copernicus_marine.open_dataset(
         dataset_id = ds_id,
@@ -15,11 +17,11 @@ def get_coords_corrected_cli(du):
         maximum_longitude = du.LONGITUDE.values+.2,
         minimum_latitude = du.LATITUDE.values-.2,
         maximum_latitude = du.LATITUDE.values+.2,
-        start_datetime = '1993-01-01',
-        end_datetime = '1993-12-31',
+        start_datetime = '2000-01-01',
+        end_datetime = '2020-12-31',
         variables = ["uo","vo"]
-    )    
-    vel_glo = vel_glo.mean('time')
+    )
+    vel_glo = vel_glo.mean('time')    
     ascent_duration = int((du.PROFILE_DATE - du.GROUNDING_DATE).values)/(1e9)
     uom = -1*ascent_duration*vel_glo.where(vel_glo['depth']<du.GROUNDING_DEPTH.values,drop=True)['uo'].interp(latitude=du['LATITUDE'].values,
                                                                                                               longitude=du['LONGITUDE'].values).mean('depth').values
@@ -37,10 +39,10 @@ lat_c2 = np.zeros(len(df1.N_GRD.values))
 
 for i in df1.N_GRD.values:
     try:
-        lon_c2[i],lat_c2[i] = get_coords_corrected_cli(df1.isel(N_GRD=i))
+        lon_c2[i],lat_c2[i] = get_coords_corrected_glo(df1.isel(N_GRD=i))
     except:
         print("some kind of error with ",i)           
-    if(i%100==0):
+    if(i%50==0):
         print(i)
 
 df1['LATITUDE_C2'] = xr.DataArray(lat_c2,dims='N_GRD')
@@ -51,4 +53,4 @@ df1['LATITUDE_C2'].attrs['long_name'] = 'Corrected latitude from Glorys climatol
 df1['LONGITUDE_C2'].attrs = df1['LATITUDE'].attrs
 df1['LONGITUDE_C2'].attrs['long_name'] = 'Corrected longitude from Glorys climatology velocities'
 
-df1.to_netcdf('working_ds_third_positions.nc')
+df1.to_netcdf('working_ds_fourth_positions.nc')
